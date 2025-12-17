844e810734e7f574c09268f934e203ed07c302d9
thyagaraaj-oss <thyagaraaj124@gmail.com>
2025-12-18 01:49:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentdb")
+.then(() => console.log("MongoDB Connected"));
+
+app.use("/api/students", require("./routes/studentRoutes"));
+
+app.listen(5000, () => console.log("Server running on 5000"));
